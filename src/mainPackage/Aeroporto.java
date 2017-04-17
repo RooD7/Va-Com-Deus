d2fd0bcbe8d3b3793e85cc0240baa98c0ce2600e
RooD7 <rodrigosoualves7@gmail.com>
2017-04-17 16:45:04 -0300

    Função - Voos por Mes

--- src/mainPackage/Aeroporto.java
+++ src/mainPackage/Aeroporto.java
@@ -1,8 +1,10 @@
-//package mainPackage;
+package mainPackage;
+import java.util.Date;
+import java.util.Calendar;
 
  public class Aeroporto {
 
- 	private Companhia[] listComp = new Companhia[1000];
+ 	private Companhia[] listComp = new Companhia[100];
  	private int tamListComp;
 
     //comitar essa bosta
@@ -29,8 +31,47 @@
 	}
 
 	//Voo's Mes cada Companhia realizou
+	public int VoosMesCompanhia(Companhia comp, int numMes) {
+		Calendar c = Calendar.getInstance();
+		Date dataAtual = new Date();
+		Date dataInicial;
+		Date dataFinal;
+		Date dataVoo;
+		int qntdeVooMes = 0;
+
+		// calendar recebe data atual
+		c.setTime(dataAtual);
+		
+		// dataInicial = 01/numMes/20**
+		c.set(Calendar.DAY_OF_MONTH, "01");
+		c.set(Calendar.MONTH, numMes);
+		dataInicial = c.getTime();
+
+		// dataFinal = (29-30-31)/numMes/20**
+		c.set(Calendar.MONTH, numMes+1);
+		c.set(Calendar.DAY_OF_MONTH, c.get(Calendar.DAY_OF_MONTH) - 1);
+		dataFinal = c.getTime();
+
+		System.out.print("Data Inicial: "+dataInicial.toString()+"\tData Final: "+dataFinal.toString()+"\n");
+		
+		//percorre a lista de voos
+		Voo[] lista = comp.getListVoo();
+		for (int i=0; lista[i] != null; i++) {
+			dataVoo = lista[i].getDataVoo();
+			//voo's que pertence aquele mes(dataAtual- 1 mes)
+			if(dataVoo.after(dataInicial) && dataVoo.before(dataFinal)) {
+				if((lista[i].getInfoVoo() == "INCLUIDO") && (lista[i].getStatusVoo() == "CONFIRMADO")) {
+					qntdeVooMes++;
+				}
+			}
+		}
+		return qntdeVooMes;
+	}
 
 	//Lucro por Voo's
+	public double LucroVoo() {
+		
+	}
 
 	//Tratar limite de 4 decolagens a cada 30 min
 
